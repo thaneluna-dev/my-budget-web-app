f64440ef3deb784605961087044f72a21f2d2c31
Thaneluna <42688917+Thaneluna@users.noreply.github.com>
2026-07-28 08:46:58 -1000

    Created badge notification bar population

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,9 @@
 import "../index.css";
 {/* // 1. Navigation Menu Comopnent ---- */}
 export default function Navbar() {
+    // Items here is for the notification bar to show any updates
+    // Most likely will be a fetch to the table that has all acitivties that have happened since last refresh
+    const items = []
   return (
     <div className="navbar bg-pink-200 shadow-sm">
       <div className="navbar-start">
@@ -84,7 +87,8 @@ export default function Navbar() {
                 d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
               />{" "}
             </svg>
-            <span className="badge badge-xs badge-primary indicator-item"></span>
+            {items.length > 0 ? 
+            <span className="badge badge-xs badge-primary indicator-item"></span> : null}
           </div>
         </button>
       </div>
