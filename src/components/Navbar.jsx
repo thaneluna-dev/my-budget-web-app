e6931a84a6b8081ad32ed8f56766f8cd062ea948
Thaneluna <42688917+Thaneluna@users.noreply.github.com>
2026-07-31 15:37:12 -1000

    Updated navbar colors

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,11 +9,12 @@ import {
 } from "lucide-react";
 import "../index.css";
 import { Navigate, useNavigate } from "react-router-dom";
+import budgetIcon from "../assets/money-3.png";
 
 {
   /* // 1. Navigation Menu Comopnent ---- */
 }
-export default function Navbar({isMobile}) {
+export default function Navbar({ isMobile }) {
   // Items here is for the notification bar to show any updates
   // Most likely will be a fetch to the table that has all acitivties that have happened since last refresh
   const navigate = useNavigate();
@@ -40,17 +41,52 @@ export default function Navbar({isMobile}) {
   ];
   return (
     <nav className="fixed bottom-5 left-1/2 -translate-x-1/2 z-50 flex  lg:h-full lg:left-0 lg:translate-x-0 lg:z-0 lg:bottom-0">
-      <div className="rounded-full lg:rounded-none lg:h-full bg-gray-300 p-2 lg:p-10 h-12 content-center lg:content-start">
-        <div className="flex text-black gap-10 px-5 lg:px-0">
-          <a className="lg:cursor-pointer" onClick={() => window.location.href="/dashboard"}>
-            <LayoutDashboard />
-          </a>
-          <a className="lg:cursor-pointer" onClick={() => window.location.href="/transactions"}>
-            <Wallet />
-          </a>
-          <a className="lg:cursor-pointer" onClick={() => window.location.href="/budget"}>
-            <DollarSign />
-          </a>
+      <div className="rounded-full lg:rounded-none lg:h-full bg-gray-700 p-2 lg:p-10 h-12 content-center lg:content-start">
+        <div className="flex text-white gap-10 lg:px-0 lg:flex-col lg:gap-7 lg:w-full">
+          <div className="hidden lg:inline-flex font-bold gap-5 place-items-center">
+            <img
+              src={budgetIcon}
+              alt="Our Budget App"
+              className="lg:w-10 lg:h-10 lg:cursor-pointer"
+            />
+            <h1 id="title">Our Budget App</h1>
+          </div>
+          {isMobile ? (
+            <a
+              className="lg:cursor-pointer"
+              onClick={() => (window.location.href = "/dashboard")}
+            >
+              <LayoutDashboard size={24} />
+            </a>
+          ) : (
+            <button className="text-left hover:bg-gray-200  cursor-pointer p-2 rounded-[10px]">
+              Dashboard
+            </button>
+          )}
+          {isMobile ? (
+            <a
+              className="lg:cursor-pointer"
+              onClick={() => (window.location.href = "/dashboard")}
+            >
+              <Wallet size={24} />
+            </a>
+          ) : (
+            <button className="text-left hover:bg-gray-200  cursor-pointer p-2 rounded-[10px]">
+              Transactions
+            </button>
+          )}
+          {isMobile ? (
+            <a
+              className="lg:cursor-pointer"
+              onClick={() => (window.location.href = "/dashboard")}
+            >
+              <DollarSign size={24} />
+            </a>
+          ) : (
+            <button className="text-left hover:bg-gray-200 cursor-pointer p-2 rounded-[10px]">
+              Budget
+            </button>
+          )}
         </div>
       </div>
     </nav>
